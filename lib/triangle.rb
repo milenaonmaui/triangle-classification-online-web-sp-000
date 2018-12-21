6fef263b68a5a9175e7642085c18b560258cf32c
Milena Kari <milenakari@yahoo.com>
2018-12-21 20:34:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-    if (@side1 + @side2 < @side3) || (@side2 + @side3 < @side1) || (@side1 + @side3 < @side2)
+    if (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       begin
         raise TriangleError
         rescure TriangleError => error
